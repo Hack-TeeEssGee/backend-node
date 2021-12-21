93efa5c9b36d3e86305058926234c39905a2f0af
rajivharlalka <rajivharlalka009@gmail.com>
2021-12-21 10:03:39 +0530

    setup winston and morgan

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const Logger = require("./utils/logger");
 
 const app = express();
 app.use(cors());
 
+dotenv.config();
 
+app.use(morgan("tiny"));
 
 const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-    res.send("KGPverse api service working.");
+  res.send("KGPverse api service working.").status(200);
 });
 
 app.listen(port, () => {
-    console.log(`App started. Listening on port ${port}`);
+  Logger.info(`App started. Listening on port ${port}`);
 });
