785a1c0791f91f267fca4e202b2f088e610712f1
rajivharlalka <rajivharlalka009@gmail.com>
2021-12-22 14:28:38 +0530

    Apdated connection settings

--- utils/connection.js
+++ utils/connection.js
@@ -2,8 +2,9 @@ const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const sequelize = new Sequelize(process.env.DATABASE_STRING);
+
 const connectDB = async () => {
-  const sequelize = new Sequelize(process.env.DATABASE_STRING);
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
@@ -17,5 +18,6 @@ const closeDB = () => {
   sequelize.close();
 };
 
+exports.sequelize = sequelize;
 exports.connectDB = connectDB;
 exports.closeDB = closeDB;
