1bffe3adbb0bcfbca9b56a9f2e6d59fe5694b226
rajivharlalka <rajivharlalka009@gmail.com>
2021-12-22 14:29:18 +0530

    Synced db with new models created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const Logger = require("./utils/logger");
-const { connectDB, closeDB } = require("./utils/connection");
+const { connectDB, closeDB, sequelize } = require("./utils/connection");
 
 const app = express();
 app.use(cors());
@@ -11,11 +11,19 @@ dotenv.config();
 app.use(morgan("tiny"));
 
 const testRoute = require("./routes/test");
-
-const port = process.env.PORT || 8000;
+const Student = require("./models/Student");
+const  Event= require("./models/Event");
 
 app.use("/", testRoute);
 
-app.listen(port, () => {
-  Logger.info(`App started. Listening on port ${port}`);
-});
+const port = process.env.PORT || 8000;
+sequelize
+  .sync()
+  .then(
+    app.listen(port, () => {
+      Logger.info(`App started. Listening on port ${port}`);
+    })
+  )
+  .catch((err) => {
+    console.log(err);
+  });
