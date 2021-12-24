92fe7596b100eb28f89675c9290394b18f669244
rajivharlalka <rajivharlalka009@gmail.com>
2021-12-24 21:54:13 +0530

    Unintended import removed

--- controllers/test.js
+++ controllers/test.js
@@ -1,7 +1,3 @@
-const { connectDB, closeDB } = require("../utils/connection");
-
 exports.testDB = (req, res) => {
-  connectDB();
-  closeDB();
   res.send("KGPverse api service working.").status(200);
 };
