fa80b03132b103ab0f1b67adf97e9b6eed7caf16
rajivharlalka <rajivharlalka009@gmail.com>
2021-12-24 13:52:28 +0530

    Added the OTP model

--- utils/connection.js
+++ utils/connection.js
@@ -5,15 +5,17 @@ const Logger = require("../utils/logger");
 //Models Imports
 // const Student = require("./models/Student");
 const { Event_Model } = require("../models/Event");
+const { OTP_Model } = require("../models/OTP");
 
 const sequelize = new Sequelize(process.env.DATABASE_STRING, {
   logging: false,
 });
 
 const Event = Event_Model(sequelize, Sequelize);
+const OTP = OTP_Model(sequelize, Sequelize);
 
 sequelize.sync().then(() => {
   Logger.info("db and tables have been created");
 });
 
-module.exports = { Event };
+module.exports = { Event, OTP };
