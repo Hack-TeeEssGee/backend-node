ddc9b6840a8aa49007fadf0fd2d8bacb7439d740
rajivharlalka <rajivharlalka009@gmail.com>
2021-12-28 09:45:43 +0530

    Certificate model made

---
+++ models/Certificate.js
@@ -0,0 +1,18 @@
+exports.Certificate_Model = function (sequelize, DataTypes) {
+    return sequelize.define(
+        "CERTIFICATE",
+        {
+            id: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                primaryKey: true,
+            },
+            location: DataTypes.STRING,
+            fileName: DataTypes.STRING,
+        },
+        {
+            tableName: "certificate",
+            timestamps: false,
+        }
+    );
+};
