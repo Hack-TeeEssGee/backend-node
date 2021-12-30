0436ceebe3610e41be18d071e322e6951e9c0f7f
rajivharlalka <rajivharlalka009@gmail.com>
2021-12-30 16:49:51 +0530

    Event response updated

--- controllers/event.js
+++ controllers/event.js
@@ -2,7 +2,7 @@ const {Event} = require("../utils/connection");
 
 exports.uploadEvent = async (req, res) => {
     try {
-        const {name,fb_post_link} = req.body;
+        const {name, fb_post_link} = req.body;
 
         const event_instance = await Event.create({
             location: req.file.location,
@@ -10,7 +10,8 @@ exports.uploadEvent = async (req, res) => {
             name: name,
         });
 
-        res.send("Event Created");
+        const response = {Status: "Success", Details: "Event Created"};
+        return res.status(200).send(response);
     } catch (err) {
         const response = {Status: "Failure", Details: err.message};
         return res.status(400).send(response);
