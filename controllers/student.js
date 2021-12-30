69c284d9c062059aea5fae85ac563a6392d7b8a3
rajivharlalka <rajivharlalka009@gmail.com>
2021-12-30 23:12:21 +0530

    getCertificateList controller

---
+++ controllers/student.js
@@ -0,0 +1,24 @@
+const {Student, Events, Certificate} = require("../utils/connection");
+const uuid = require("uuid");
+
+exports.getCertificateList = async (req, res) => {
+    try {
+        const {email} = req.body;
+
+        const student_instance = await Student.findOne({where: {email}});
+
+        const certificate_list = JSON.parse(student_instance.certificates);
+        const certificate_instance = await Certificate.findOne({where: {id: certificate_list[0]}});
+        let event_details = [];
+        for (e of certificate_list) {
+            const event_instance = await Events.findOne({where: {id: certificate_instance.event}});
+            event_details.push({name: event_instance.name, link: event_instance.location});
+        }
+
+        const response = {Status: "Success", Details: "Students Certificate details fetched", event_details};
+        return res.status(200).send(response);
+    } catch (err) {
+        const response = {Status: "Failure", Details: err.message};
+        return res.status(400).send(response);
+    }
+};
