c5d719cbe16a447bec1fb8ac46350637b31a6a48
rajivharlalka <rajivharlalka009@gmail.com>
2022-01-08 19:24:23 +0530

    Add the add Society and uplload bill controllers

--- controllers/society.js
+++ controllers/society.js
@@ -1,4 +1,4 @@
-const {Society} = require("../utils/connection");
+const {Society, Bill} = require("../utils/connection");
 
 exports.getAllSociety = async (req, res) => {
     try {
@@ -6,6 +6,44 @@ exports.getAllSociety = async (req, res) => {
 
         const response = {Status: "Success", Details: "All Socities Loaded", societies};
         return res.status(200).send(response);
+    } catch (err) {
+        const response = {Status: "Failure", Details: err.message};
+        return res.status(400).send(response);
+    }
+};
+
+exports.addSociety = async (req, res) => {
+    try {
+        const {name, description} = req.body;
+
+        await Society.create({
+            name,
+            description,
+            logo: req.file.location,
+        });
+        
+        const response = {Status: "Success", Details: "Society Created"};
+        return res.status(200).send(response);
+    } catch (err) {
+        const response = {Status: "Failure", Details: err.message};
+        return res.status(400).send(response);
+    }
+};
+
+exports.uploadBill = async (req, res) => {
+    try {
+        const {name, amount, description} = req.body;
+
+        await Bill.create({
+            key: req.file.key,
+            name,
+            amount,
+            description,
+        });
+
+        const response = {Status: "Success", Details: "Bill Uploaded"};
+        return res.status(200).send(response);
+        res.send("hello world");
     } catch (err) {
         const response = {Status: "Failure", Details: err.message};
         return res.status(400).send(response);
