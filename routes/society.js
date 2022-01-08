7dcb02d34adc7346f46fe92d0a882b9056bc9ac7
rajivharlalka <rajivharlalka009@gmail.com>
2022-01-08 19:24:56 +0530

    Add society route

--- routes/society.js
+++ routes/society.js
@@ -1,7 +1,15 @@
-const { getAllSociety } = require("../controllers/society");
+const {getAllSociety, uploadBill, addSociety} = require("../controllers/society");
+const {upload_bill, upload_society_logo} = require("../utils/middleware/multer");
 
 const router = require("express").Router();
 
-router.get("/",getAllSociety)
+router.get("/", getAllSociety);
+// create a society
+router.post("/", upload_society_logo.single("image"), addSociety);
+//upload a event
+//get event of partcular society
+//upload bill
+//get all bills
+router.post("/uploadbill", upload_bill.single("bill"), uploadBill);
 
 module.exports = router;
