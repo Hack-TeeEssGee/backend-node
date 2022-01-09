18c30add13a61d7bcb7e343bebeccc5636bbcc8c
rajivharlalka <rajivharlalka009@gmail.com>
2022-01-09 15:46:56 +0530

    changed event routes

--- routes/event.js
+++ routes/event.js
@@ -2,14 +2,16 @@ const router = require("express").Router();
 const {uploadTSGEvent, getAllEvents, getTodaysEvents, uploadSocEvent} = require("../controllers/event");
 const {upload_event} = require("../utils/middleware/multer");
 
-router.post("/tsg/upload", upload_event.single("image"), uploadTSGEvent);
+router.post("/upload/tsg", upload_event.single("image"), uploadTSGEvent);
+
 router.get("/", getAllEvents);
+
 router.post("/today", getTodaysEvents);
 
 //get event of partcular society
 router.get("/:id", getSocietyEvents);
 
 //upload society event
-router.post("/society/upload", upload_event.single("image"), uploadSocEvent);
+router.post("/upload/society", upload_event.single("image"), uploadSocEvent);
 
 module.exports = router;
