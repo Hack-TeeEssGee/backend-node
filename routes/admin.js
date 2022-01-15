af429310ffe993ce19a9cf701d495306982d9856
rajivharlalka <rajivharlalka009@gmail.com>
2022-01-15 22:38:08 +0530

    add admin and tsg route for get grienvenc controller

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const {updateBill} = require("../controllers/admin");
+const {getSolvedGrievence, getPendingGrievence} = require("../controllers/grievence");
 
-router.put("/bill",updateBill)
+router.put("/bill", updateBill);
+router.get("/solved/grievence", getSolvedGrievence);
+router.get("/pending/grievence", getPendingGrievence);
 
 module.exports = router;
