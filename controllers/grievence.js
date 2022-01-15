1b6dc9c30baed8748eef6b95b82d6ede7a2374eb
rajivharlalka <rajivharlalka009@gmail.com>
2022-01-15 22:37:42 +0530

    Add get solved and pening grievence

--- controllers/grievence.js
+++ controllers/grievence.js
@@ -1,4 +1,5 @@
 const {Grievence} = require("../utils/connection");
+const {Op} = require("sequelize");
 
 exports.uploadGrievence = async (req, res) => {
     try {
@@ -12,3 +13,28 @@ exports.uploadGrievence = async (req, res) => {
         return res.status(400).send(response);
     }
 };
+
+
+exports.getSolvedGrievence=async (req,res)=>{
+    try{
+        const grievences=await Grievence.findAll({where:{state: "Solved"}});
+
+        const response = {Status: "Success", Details: "All Solved Grievence Returned", response:grievences};
+        return res.status(200).send(response);
+    }catch (err) {
+        const response = {Status: "Failure", Details: err.message};
+        return res.status(400).send(response);
+    }
+}
+
+exports.getPendingGrievence=async (req,res)=>{
+    try{
+        const grievences=await Grievence.findAll({where:{state: "Pending"}});
+
+        const response = {Status: "Success", Details: "All Pending Grievence Returned", response:grievences};
+        return res.status(200).send(response);
+    }catch (err) {
+        const response = {Status: "Failure", Details: err.message};
+        return res.status(400).send(response);
+    }
+}
