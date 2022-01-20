54dcfe45db7bd5d97e09536f8594629cd185a132
rajivharlalka <rajivharlalka009@gmail.com>
2022-01-20 23:54:23 +0530

    getOffcieBearer contrcutor add in class

--- utils/sheets.js
+++ utils/sheets.js
@@ -44,25 +44,30 @@ class Sheet {
         }
         throw new Error("No such user found");
     }
-    async find() {
+    async findOfficialsData() {
         const gsapi = google.sheets({version: "v4", auth: cl});
         const opt = {
             spreadsheetId: this.sheetId,
-            range: "2021",
+            range: "TSG Officials",
         };
         let dataObtained = await gsapi.spreadsheets.values.get(opt);
         const rows = dataObtained.data.values;
-        for (let row of rows) {
-            if (row[2] === email) {
-                console.log(row);
-                let response = {};
-                for (const heading in rows[0]) {
-                    response[rows[0][heading]] = row[heading];
+
+        let data = {"Current Office Bearers": [], "TSG Staff": [], Secretary: []};
+        try {
+            for (let row_no = 1; row_no < rows.length; row_no++) {
+                let row = rows[row_no];
+                let obj = {};
+                for (let i = 1; i < row.length; i++) {
+                    obj[rows[0][i]] = row[i];
                 }
-                return response;
+                console.log(obj);
+                data[row[0]].push(obj);
             }
+            return data;
+        } catch (err) {
+            throw new Error(err.message);
         }
-        throw new Error("No such user found");
     }
 }
 
