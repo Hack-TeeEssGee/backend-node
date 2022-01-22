89ca8843c0c7e70683a71869b0d428f79f6b2327
rajivharlalka <rajivharlalka009@gmail.com>
2022-01-22 13:12:35 +0530

    add blog routes

--- routes/info.js
+++ routes/info.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
+const {getAllBlogs, addBlog, reportBlog, deleteBlog} = require("../controllers/blog.controller");
 const {getHallInfo} = require("../controllers/info");
 
 router.get("/hall", getHallInfo);
 
+router.get("/blog", getAllBlogs);
+router.post("/blog", addBlog);
+router.post("/blog/report", reportBlog);
+router.delete("/blog", deleteBlog);
+
 module.exports = router;
+blog
