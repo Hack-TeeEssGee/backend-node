ede128f64a5f061111596ce2d2a637eb720799a0
rajivharlalka <rajivharlalka009@gmail.com>
2022-01-23 01:49:00 +0530

    Add multer middleware for news image upload

--- utils/middleware/multer.js
+++ utils/middleware/multer.js
@@ -98,6 +98,19 @@ exports.upload_tsg_image = multer({
     }),
 });
 
+exports.upload_news_image = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.PUBLIC_BUCKET,
+        metadata: function (req, file, cb) {
+            cb(null, {fieldName: file.fieldname});
+        },
+        key: function (req, file, cb) {
+            cb(null, "news/" + file.originalname);
+        },
+    }),
+});
+
 exports.getFileFromS3 = key => {
     const downloadParams = {
         Key: key,
