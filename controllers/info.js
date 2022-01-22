ccf511dd982b705e9c4efe889dd36b936f79074d
rajivharlalka <rajivharlalka009@gmail.com>
2022-01-22 23:48:25 +0530

    get Prof Data controller made

--- controllers/info.js
+++ controllers/info.js
@@ -40,3 +40,16 @@ exports.getInterIITEvents = async (req, res) => {
         return res.status(400).send(response);
     }
 };
+
+
+exports.getProfData=async (req,res)=>{
+    try {
+        const sheet = new Sheet(sheetID.student_sheet, "Professor");
+        const data = await sheet.find();
+        const response = {Status: "Success", Details: "All GC events Loaded", data};
+        return res.status(200).send(response);
+    } catch (err) {
+        const response = {Status: "Failure", Details: err.message};
+        return res.status(400).send(response);
+    }
+}
--- routes/info.js
+++ routes/info.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const {getAllBlogs, addBlog, reportBlog, deleteBlog} = require("../controllers/blog.controller");
-const {getHallInfo, getGCEvents, getInterIITEvents} = require("../controllers/info");
+const {getHallInfo, getGCEvents, getInterIITEvents, getProfData} = require("../controllers/info");
 
 router.get("/hall", getHallInfo);
 router.get("/gc", getGCEvents);
 router.get("/interiit", getInterIITEvents);
+router.get("/professor", getProfData);
 
 router.get("/blog", getAllBlogs);
 router.post("/blog", addBlog);
