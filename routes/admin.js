33081e58d9bd60102546a8f2a1b915a126b31202
rajivharlalka <rajivharlalka009@gmail.com>
2022-01-23 01:49:13 +0530

    news upload route made

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const {updateBill, downloadBill} = require("../controllers/admin");
 const {getSolvedGrievence, getPendingGrievence, updateGrievenceStatus} = require("../controllers/grievence");
-const {getAllNews} = require("../controllers/news");
+const {getAllNews, uploadNews} = require("../controllers/news");
+const {upload_news_image} = require("../utils/middleware/multer");
 
 router.put("/bill", updateBill);
 router.get("/solved/grievence", getSolvedGrievence);
@@ -9,5 +10,6 @@ router.get("/pending/grievence", getPendingGrievence);
 router.put("/grievence", updateGrievenceStatus);
 router.get("/bill/:id", downloadBill);
 router.get("/news", getAllNews);
+router.post("/news", upload_news_image.single("image"), uploadNews);
 
 module.exports = router;
